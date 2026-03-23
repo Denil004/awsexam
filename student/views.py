142a0927925ed4578c8964d99cb2c5429869441d
albert <albertcyriac04@gmail.com>
2026-03-23 16:44:16 +0530

    Add full CRUD functionality for Student normal app

--- student/views.py
+++ student/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
 from .models import Student
 
@@ -7,8 +7,24 @@ class StudentListView(ListView):
     template_name = 'student/student_list.html'
     context_object_name = 'students'
 
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'student/student_detail.html'
+    context_object_name = 'student'
+
 class StudentCreateView(CreateView):
     model = Student
     fields = ['first_name', 'last_name', 'email', 'grade']
     template_name = 'student/student_form.html'
+    success_url = reverse_lazy('student_list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ['first_name', 'last_name', 'email', 'grade']
+    template_name = 'student/student_form.html'
+    success_url = reverse_lazy('student_list')
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'student/student_confirm_delete.html'
     success_url = reverse_lazy('student_list')
